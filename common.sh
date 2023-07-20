08cddf392d29b3c3b1180ae0cfc13b08dcc3e26d
bootli <106524425+bootli@users.noreply.github.com>
2023-07-20 13:01:14 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -296,14 +296,13 @@ COOLSNOWWOLF)
   s="luci-app-netdata,netdata,luci-app-diskman,mentohust"
   c=(${s//,/ })
   for i in ${c[@]}; do \
-    find . -type d -name 'luci-theme-argon' -o -name 'luci-theme-argonv3' -o -name 'luci-theme-argon-mod' -o -name 'luci-app-argon-config' -o -name 'luci-app-v2ray-server' -o -name 'luci-app-netdata' -o -name 'luci-app-samba4' | xargs -i rm -rf {}
-    find . -type d -name "r8168" -o -name "r8101" -o -name "r8125" |grep 'danshui' |xargs -i rm -rf {}
     find . -type d -name "${i}" |grep -v 'danshui' |xargs -i rm -rf {}; \
-
   done
  
 
   if [[ "${GL_BRANCH}" == "lede" ]]; then
+    find . -type d -name 'luci-theme-argon' -o -name 'luci-theme-argonv3' -o -name 'luci-theme-argon-mod' -o -name 'luci-app-argon-config' -o -name 'luci-app-v2ray-server' -o -name 'luci-app-netdata' -o -name 'autosamba' -o -name 'luci-app-samba4' | xargs -i rm -rf {}
+    find . -type d -name "r8168" -o -name "r8101" -o -name "r8125" |grep 'danshui' |xargs -i rm -rf {}
     git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon "${HOME_PATH}/feeds/luci/themes/luci-theme-argon"
     git clone -b master https://github.com/jerrykuku/luci-theme-argon "${HOME_PATH}/feeds/luci/themes/luci-theme-argonv3"
     git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config "${HOME_PATH}/feeds/luci/applications/luci-app-argon-config"

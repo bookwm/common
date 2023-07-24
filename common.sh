5c4dfb6175e6de00d4a7da2eaaa2c4abde012055
bootli <106524425+bootli@users.noreply.github.com>
2023-07-24 17:19:42 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -301,13 +301,14 @@ COOLSNOWWOLF)
  
 #lede源自己添加些插件
   if [[ "${GL_BRANCH}" == "lede" ]]; then
-    find . -type d -name 'luci-theme-argon' -o -name 'luci-theme-argonv3' -o -name 'luci-theme-argon-mod' -o -name 'luci-app-argon-config' -o -name 'luci-theme-argone-mod' -o -name 'luci-app-argone-config' -o -name 'luci-app-v2ray-server' -o -name 'luci-app-netdata' -o -name 'autosamba' -o -name 'luci-app-samba4' -o -name 'luci-app-ikoolproxy' | xargs -i rm -rf {}
+    find . -type d -name 'luci-theme-argon' -o -name 'luci-theme-argonv3' -o -name 'ddns-go' -o -name 'luci-app-ddns-go' -o -name 'luci-theme-argon-mod' -o -name 'luci-app-argon-config' -o -name 'luci-theme-argone-mod' -o -name 'luci-app-argone-config' -o -name 'luci-app-v2ray-server' -o -name 'luci-app-netdata' -o -name 'autosamba' -o -name 'luci-app-samba4' -o -name 'luci-app-ikoolproxy' | xargs -i rm -rf {}
 #find . -type d -name "r8168" -o -name "r8101" -o -name "r8125" |grep 'danshui' |xargs -i rm -rf {}
     git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon "${HOME_PATH}/package/lean/luci-theme-argon"
     git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config "${HOME_PATH}/package/lean/luci-app-argon-config"
-    git clone -b master https://github.com/bootli/luci-app-v2ray-server "${HOME_PATH}/feeds/luci/applications/luci-app-v2ray-server"
-    git clone -b master https://github.com/bootli/luci-app-samba4 "${HOME_PATH}/feeds/luci/applications/luci-app-samba4"
-    git clone -b master https://github.com/bootli/libuild "${HOME_PATH}/package/libuild"
+    git clone https://github.com/bootli/luci-app-v2ray-server "${HOME_PATH}/feeds/luci/applications/luci-app-v2ray-server"
+    git clone https://github.com/bootli/luci-app-samba4 "${HOME_PATH}/feeds/luci/applications/luci-app-samba4"
+    git clone https://github.com/bootli/libuild "${HOME_PATH}/package/libuild"
+    git clone https://github.com/sirpdboy/luci-app-ddns-go "${HOME_PATH}/package/ddns-go"
     
   if [[ ! -f "${HOME_PATH}/target/linux/ramips/mt7621/config-5.15" ]]; then
       for i in "mt7620" "mt7621" "mt76x8" "rt288x" "rt305x" "rt3883"; do \
